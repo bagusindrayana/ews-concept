b403997f3289dd967d3fe8d9163b51b64612b1b6
bagusindrayana <bagusindrayanaindo@gmail.com>
2026-03-09 11:03:50 +0800

    update package and turbopack

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,29 @@
 import withBundleAnalyzer from '@next/bundle-analyzer';
+
 const bundleAnalyzer = withBundleAnalyzer({
-	enabled: process.env.ANALYZE === 'true',
+  enabled: process.env.ANALYZE === 'true',
 })
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config) {
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: ['@svgr/webpack'],
-        });
-        return config;
-    },
-    async rewrites() {
-        return [
-          {
-            source: '/socket.io/:path*',
-            destination: '/api/seedlink',
-          },
-        ];
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/socket.io/:path*',
+        destination: '/api/seedlink',
       },
+    ];
+  },
+
+  turbopack: {
+
+  }
 };
 
 
